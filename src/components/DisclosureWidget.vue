4604605d76803819a44a06a135d46ba3d2c6b2c0
Mark Noonan <mark@cypress.io>
2022-02-05 20:44:17 -0500

    wip custom disclosure

--- src/components/DisclosureWidget.vue
+++ src/components/DisclosureWidget.vue
@@ -1,12 +1,106 @@
 <template>
-    <details :data-cy-component="$options.name">
-        <summary><slot name="title">Untitled Disclosure 😱</slot></summary>
-        <slot>Details with no details 😬</slot>
-    </details>
+    <div :data-cy-component="$options.name">
+        <!-- based on https://adrianroselli.com/2020/05/disclosure-widgets.html -->
+        <button
+            type="button"
+            :aria-expanded="expanded"
+            @click="toggleDisclosure"
+            :aria-controls="id"
+        >
+            <span>
+                <svg viewBox="0 0 80 80" focusable="false">
+                    <path d="M70.3 13.8L40 66.3 9.7 13.8z" />
+                </svg>
+            </span>
+            <slot name="title">Untitled Disclosure 😱</slot>
+        </button>
+        <div :id="id" class="disclosee">
+            <p>
+                <slot>Details with no details 😬</slot>
+            </p>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'DisclosureWidget'
+    name: 'DisclosureWidget',
+    data() {
+        return {
+            id: 'widget-' + Math.floor(100000 + Math.random() * 900000),
+            expanded: false
+        }
+    },
+    methods: {
+        toggleDisclosure() {
+            this.expanded = !this.expanded
+        }
+    }
 }
 </script>
+
+<style scoped>
+button[aria-expanded="false"] + div {
+  display: none;
+}
+
+button[aria-expanded] {
+  border: .1em solid #ccc;
+  padding: .5em 1em .5em .5em;
+  font: inherit;
+  background-color: #eee;
+  border-radius: .5em .5em 0 0;
+}
+
+button[aria-expanded] span {
+  display: inline-block;
+  font-size: 60%;
+  color: #000;
+  background-color: #00f;
+  padding: 0.3em 0.2em 0 0.2em;
+  border: 0.2em solid #00f;
+  border-radius: 50%;
+  line-height: 1;
+  text-align: center;
+  text-indent: 0;
+  transform: rotate(270deg);
+}
+
+button[aria-expanded] svg {
+  width: 1.25em;
+  height: 1.25em;
+  fill: #fff;
+  transition: transform 0.25s ease-in;
+  transform-origin: center 45%;
+}
+
+button[aria-expanded]:hover,
+button[aria-expanded]:focus {
+
+  outline: none;
+  border-color: #666;
+}
+
+button[aria-expanded]:hover span,
+button[aria-expanded]:focus span {
+  background-color: #fff;
+  outline: none;
+}
+
+button[aria-expanded]:hover svg,
+button[aria-expanded]:focus svg {
+  fill: #00f;
+}
+
+/* Lean on programmatic state for styling */
+button[aria-expanded="true"] svg {
+  transform: rotate(90deg);
+}
+
+.disclosee {
+  padding: .25em .75em;
+  border: .1em solid #ddd;
+  background-color: #fff;
+  margin-top: -.1em;
+}
+</style>
--- src/views/OtherPlace.vue
+++ src/views/OtherPlace.vue
@@ -1,11 +1,28 @@
 <template>
-<div :data-cy-component="$options.name">
-    Some Other Place
-</div>
+    <div :data-cy-component="$options.name">
+        <h1>Some Other Place</h1>
+        <DisclosureWidget>
+            <template #title>
+                <h2>Outer Disclosure Title</h2>Outer body
+            </template>
+
+            <DisclosureWidget>
+                <template #title>
+                    <h3>Inner Disclosure Title</h3>Inner Disclosure Body
+                </template>
+            </DisclosureWidget>
+        </DisclosureWidget>
+    </div>
 </template>
 
 <script>
+
+import DisclosureWidget from "../components/DisclosureWidget.vue"
+
 export default {
     name: "OtherPlace",
+    components: {
+        DisclosureWidget
+    }
 }
 </script>
