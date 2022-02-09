dfc8b9fd03f378f62b74ad070c809fb1aaa429ab
Mark Noonan <mark@cypress.io>
2022-02-08 21:50:00 -0500

    cleanup

--- src/components/LoginForm.cy.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-import LoginForm from "./LoginForm.vue"
-
-describe('<LoginForm />', {viewportHeight: 60, viewportWidth: 300}, () => {
-  it('renders', () => {
-    cy.mount(<LoginForm />)
-    cy.validate(LoginForm.name)
-  })
-})
