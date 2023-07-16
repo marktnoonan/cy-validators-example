518687babd10748dbe1594029c1233cce8cfc2c1
marktnoonan <mark@cypress.io>
2023-07-16 12:58:36 -0400

    avoid video for component tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,30 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '69mv85',
-    'e2e': {
-        supportFile: 'cypress/support/e2e.js',
-        specPattern: 'cypress/e2e/**/*',
-        baseUrl: 'http://localhost:8080/',
-        experimentalStudio: true,
-        test: ''
+  projectId: 'fb1oy8',
+
+  // component: {
+  //     devServer:{
+  //         framework: 'vue-cli',
+  //         bundler: 'webpack',
+  //     },
+  //     defaultCommandTimeout: 500
+  // },
+  e2e: {
+    supportFile: "cypress/support/e2e.js",
+    specPattern: "cypress/e2e/**/*",
+    baseUrl: "http://localhost:8080/",
+    experimentalStudio: true,
+    experimentalRunAllSpecs: true,
+    test: "",
+    retries: 3
+  },
+
+  component: {
+    devServer: {
+      framework: "vue-cli",
+      bundler: "webpack",
     },
-    component: {
-        devServer:{
-            framework: 'vue-cli',
-            bundler: 'webpack',
-        },
-        defaultCommandTimeout: 500
-    },
+    video: false
+  },
 });
